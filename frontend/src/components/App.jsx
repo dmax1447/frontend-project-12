3148396a1dc9fee854b167bfe69ae7a777dce1ce
Max Dragaytsev <dragaytsev@air.agency>
2023-09-11 18:41:39 +0300

    form - styles

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -4,6 +4,7 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
+  Link,
 } from 'react-router-dom';
 
 import MainPage from './MainPage';
@@ -45,7 +46,7 @@ const App = () => (
       <main className="d-flex flex-column h-100">
         <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
           <div className="container">
-            <a className="navbar-brand" href="/">Hexlet CHAT</a>
+            <Link className="navbar-brand" to="/">Hexlet CHAT</Link>
           </div>
         </nav>
         <div className="container-fluid h-100">
