3e393417cc12bda653375264aff2326525d5e89f
Max Dragaytsev <dragaytsev@air.agency>
2023-09-17 17:14:27 +0300

    step4 fetch data

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -1,7 +1,26 @@
-const MainPage = () => (
-  <div>
-    Main Page
-  </div>
-);
+import { useEffect } from 'react';
+import axios from 'axios';
+
+const MainPage = () => {
+  useEffect(() => {
+    const token = localStorage.getItem('userToken');
+    axios.get('/api/v1/data', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((r) => {
+        console.log(r.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+  return (
+    <div>
+      Main Page
+    </div>
+  );
+};
 
 export default MainPage;
